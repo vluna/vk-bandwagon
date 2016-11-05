ae0628baf2520a0d46b3013858c6b8424633d6c7
vluna <vluna.0293@gmail.com>
2016-11-05 13:18:10 -0700

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name vkBandwagonApp
+ * @description
+ * # vkBandwagonApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('vkBandwagonApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vkBandwagonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vkBandwagonApp
+ */
+angular.module('vkBandwagonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
